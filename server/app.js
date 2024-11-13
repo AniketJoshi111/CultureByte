b58679178771e6b27b7d86912a03590bdaa05682
AniketJoshi111 <aniketjoshi2606@gmail.com>
2024-11-13 20:19:34 +0530

    all done

--- server/app.js
+++ server/app.js
@@ -36,9 +36,7 @@ app.post("/api/create-checkout-session",async(req,res)=>{
         success_url:"http://localhost:3000/success",
         cancel_url:"http://localhost:3000/cancel",
     });
-
     res.json({id:session.id})
- 
 })
 
 
